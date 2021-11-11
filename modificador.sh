72872cf5441c96ed4c98df70be354dc2e9f24beb
FabioPC <fabioleninpimentel@gmail.com>
2021-11-11 09:45:06 -0400

    agregar script

---
+++ modificador.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+cd /mnt/c/Users/FabioPC/Desktop/Tesis/c++/'Trabajo con Lucia'
+
+cd k=2
+
+cd L64
+cd K2L64
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L96
+cd K2L96
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L128
+cd K2L128
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L256
+cd K2L256
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L512
+cd K2L512
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L640
+cd K2L640
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd ..
+
+cd k=3
+
+cd L64
+cd K3L64
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L96
+cd K3L96
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L128
+cd K3L128
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L256
+cd K3L256
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L512
+cd K3L512
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L640
+cd K3L640
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd ..
+
+cd k=4
+
+cd L64
+cd K4L64
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L96
+cd K4L96
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L128
+cd K4L128
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L256
+cd K4L256
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L512
+cd K4L512
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L640
+cd K4L640
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd ..
+
+cd k=8
+
+cd L64
+cd K8L64
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L96
+cd K8L96
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L128
+cd K8L128
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L256
+cd K8L256
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L512
+cd K8L512
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L640
+cd K8L640
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd ..
+
+cd k=16
+
+cd L64
+cd K16L64
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L96
+cd K16L96
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L128
+cd K16L128
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L256
+cd K16L256
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L512
+cd K16L512
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L640
+cd K16L640
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd ..
+
+cd k=32
+
+cd L64
+cd K32L64
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L96
+cd K32L96
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
+
+cd L128
+cd K32L128
+sed -i '16s/.*/#define tita_ini 0.62/' main.cpp
+sed -i '17s/.*/#define tita_fin 0.59/' main.cpp
+cd ..
+cd ..
